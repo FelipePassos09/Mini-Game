80653ee75adea293a0a224b082a8fce491cd2f61
HunterStl09 <felipe.passos.09@gmail.com>
2021-06-25 23:03:52 -0300

    Criando Interface Gráfica

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
 # Tela principal, nela terá o primeiro menú para escolha do game.
 
-print('Hello World!!')
+import pygame
+import PySimpleGUI as gui
+import emoji
+
+welcome = '''
+Esse é um Mini-Game feito totalmente me python, espero que gostem.
+'''
+
+
+layout = [
+    [gui.Text('Olá!!\n{}'.format(emoji.emojize(':fogos_de_artifício:', language = 'pt')*6))],
+    [gui.Text('{}'.format(welcome))],
+    [gui.Input()],
+    [gui.Button('Escolha seu jogo.')]
+]
+window = gui.Window('Mini-Game',layout)
+event,values = window.read()
+
+
+
